0f0eae71595eaf0c744b6d1097ec0a711e612151
barkenikhil02 <barkenikhil02@gmail.com>
2021-03-20 15:22:48 +0530

    map loaded

--- lib/views/maps.dart
+++ lib/views/maps.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
   @override
@@ -6,14 +7,40 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
+  GoogleMapController myController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    myController = controller;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: Text('Maps'),
-        ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 10.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: FloatingActionButton(
+                onPressed: () => print('You have pressed the button'),
+                materialTapTargetSize: MaterialTapTargetSize.padded,
+                backgroundColor: Colors.green,
+                child: const Icon(Icons.map, size: 30.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
