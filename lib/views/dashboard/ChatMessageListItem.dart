2a3a7cc59e532641d09ca97f051edf8ebb73b815
barkenikhil02 <barkenikhil02@gmail.com>
2021-03-20 23:06:08 +0530

    chat intialized

---
+++ lib/views/dashboard/ChatMessageListItem.dart
@@ -0,0 +1,67 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+var currentUserEmail;
+
+class ChatMessageListItem extends StatelessWidget {
+  final DataSnapshot messageSnapshot;
+  final Animation animation;
+
+  ChatMessageListItem({this.messageSnapshot, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor:
+      new CurvedAnimation(parent: animation, curve: Curves.decelerate),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          children: currentUserEmail == messageSnapshot.value['email']
+              ? getSentMessageLayout()
+              : getReceivedMessageLayout(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> getSentMessageLayout() {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(messageSnapshot.value['text']),
+            ),
+          ],
+        ),
+      ),
+      new Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> getReceivedMessageLayout() {
+    return <Widget>[
+      new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(messageSnapshot.value['text']),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+}
--- lib/views/dashboard/allprojects.dart
+++ lib/views/dashboard/allprojects.dart
@@ -8,6 +8,151 @@ class Allprojects extends StatefulWidget {
 class _AllprojectsState extends State<Allprojects> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 30, 10, 10),
+        height: 220,
+        width: double.maxFinite,
+        child: Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.all(7),
+            child: Stack(children: <Widget>[
+              Align(
+                alignment: Alignment.centerRight,
+                child: Stack(
+                  children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 5),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                cryptoIcon(),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                cryptoNameSymbol(),
+                                Spacer(),
+                                cryptoChange(),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                changeIcon(),
+                                SizedBox(
+                                  width: 20,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[cryptoAmount()],
+                            )
+                          ],
+                        ))
+                  ],
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+Widget cryptoIcon() {
+  return Padding(
+    padding: const EdgeInsets.only(left: 15.0),
+    child: Align(
+        alignment: Alignment.centerLeft,
+        child: Icon(
+          Icons.favorite,
+          color: Colors.amber,
+          size: 40,
+        )),
+  );
+}
+
+Widget cryptoNameSymbol() {
+  return Align(
+    alignment: Alignment.centerLeft,
+    child: RichText(
+      text: TextSpan(
+        text: 'Bitcoin',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+        children: <TextSpan>[
+          TextSpan(
+              text: '\nBTC',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold)),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget cryptoChange() {
+  return Align(
+    alignment: Alignment.topRight,
+    child: RichText(
+      text: TextSpan(
+        text: '+3.67%',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.green, fontSize: 20),
+        children: <TextSpan>[
+          TextSpan(
+              text: '\n+202.835',
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold)),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget changeIcon() {
+  return Align(
+      alignment: Alignment.topRight,
+      child: Icon(
+        Icons.favorite,
+        color: Colors.green,
+        size: 30,
+      ));
+}
+
+Widget cryptoAmount() {
+  return Align(
+    alignment: Alignment.centerLeft,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 20.0),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            textAlign: TextAlign.left,
+            text: TextSpan(
+              text: '\n\$12.279',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 35,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                    text: '\n0.1349',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
